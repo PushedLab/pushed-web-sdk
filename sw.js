84dcbe9064e7b6a82cbafe8ad3f7924a09aee17d
Goreglyad94 <55846739+Goreglyad94@users.noreply.github.com>
2025-03-25 16:02:32 +0300

    Feature/add sw.js (#4)
    * add sw source code
    
    * increas version
    
    ---------
    
    Co-authored-by: Ярослав Горегляд <ya.goreglyad@multifactor.ru>

---
+++ sw.js
@@ -0,0 +1,71 @@
+﻿let requestPayload = null;
+
+self.addEventListener('push', function (event) {
+  const data = event.data.json() || {};
+  const image = data.Image || 'https://multipushed.ru/favicon-32x32.png';
+  const title = data.Title || '';
+  const body = data.Body || '';
+
+  requestPayload = getRequestPayload(data.WebClientToken, data.MessageId);
+
+  const options = {
+    body: body,
+    icon: image,
+    badge: image,
+    data: {
+      url: data.Url
+    }
+  };
+
+  event.waitUntil(
+      (async () => {
+        await self.registration.showNotification(title, options);
+        requestPayload && setClientInteractionStatus('Show', requestPayload);
+      })()
+  );
+
+  clients.matchAll({ includeUncontrolled: true, type: 'window' }).then(clients => {
+    data._pushed = true;
+
+    clients.forEach(client => {
+      client.postMessage(data, [new MessageChannel().port2]);
+    });
+  });
+
+  serviceWorker.dispatchEvent(new CustomEvent('message', {detail: data}));
+});
+
+self.addEventListener('notificationclick', function (event) {
+  requestPayload && setClientInteractionStatus('Click', requestPayload);
+  const url = event.notification.data.url;
+  if (url) {
+    event.waitUntil(clients.openWindow(url));
+  }
+});
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  return self.clients.claim();
+});
+
+function getRequestPayload(token, messageId) {
+  if (!token || !messageId) {
+    return null;
+  }
+
+  const headers = new Headers();
+  const base64BasicHeader = self.btoa(token + ':' + messageId);
+  headers.set('Authorization', 'Basic ' + base64BasicHeader);
+
+  return { method: 'POST', headers: headers };
+}
+
+function setClientInteractionStatus(status, requestPayload) {
+  if (status !== 'Show' && status !== 'Click' && status !== 'Close') {
+    throw Error('Wrong client interaction status');
+  }
+  fetch(`https://api.pushed.dev/v2/web-push/confirm-client-interaction?clientInteraction=${status}`, requestPayload);
+}
