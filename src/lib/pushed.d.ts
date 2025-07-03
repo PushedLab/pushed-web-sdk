f0b971c034e34cfca07f735d2a27be03031cdfee
Тимофей Алексеев <alexeev.tim@gmail.com>
2025-07-03 13:23:25 +0300

    2.0.6

--- src/lib/pushed.d.ts
+++ src/lib/pushed.d.ts
@@ -1,7 +1,7 @@
 declare module '@pushedlab/pushed-web-sdk' {
     export function registerWebPushes(userConfig?: WebPushUserConfig): Promise<string>;
     export function setNotificationListener(callback: (data: any) => void): void;
-    export function validateSubscription(): Promise<string>;
+    export function validateSubscription(userConfig?: WebPushUserConfig): Promise<string>;
     export function setApiEndpoint(): void;
     export function requestNotificationPermission(): Promise<boolean>;
     export function unsubscribeFromPush(): Promise<void>;
